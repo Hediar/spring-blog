d13c5f972d138d202d138ec5b88267f0ca5eb912
Hediar <srlimvp@gmail.com>
2025-10-12 23:45:53 +0900

    service, controller

---
+++ src/main/java/com/example/blog_api/service/BlogService.java
@@ -0,0 +1,18 @@
+package com.example.blog_api.service;
+
+import com.example.blog_api.domain.Article;
+import com.example.blog_api.dto.AddArticleRequest;
+import com.example.blog_api.repository.BlogRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
+@Service
+public class BlogService {
+    private final BlogRepository blogRepository;
+
+    // 블로그 글 추가 메서드
+    public Article save(AddArticleRequest request) {
+        return blogRepository.save(request.toEntity());
+    }
+}
