b3718616f8d8c37fa7e839de385ccef197188169
Hediar <srlimvp@gmail.com>
2025-11-08 01:56:01 +0900

    jwt setting

---
+++ src/main/java/com/example/blog_api/config/jwt/JwtProperties.java
@@ -0,0 +1,15 @@
+package com.example.blog_api.config.jwt;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Setter
+@Getter
+@Component
+@ConfigurationProperties("jwt") // java 클래스에 property값을 가져와서 사용하는 에너테이션
+public class JwtProperties {
+    private String issuer;
+    private String secretKey;
+}
---
+++ src/main/java/com/example/blog_api/config/jwt/TokenProvider.java
@@ -0,0 +1,9 @@
+package com.example.blog_api.config.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class TokenProvider {
+}
