475afdf482d746e258a73fcea406d69455eefb0d
Hediar <srlimvp@gmail.com>
2025-11-17 22:44:14 +0900

    User domain setting & Token Provider setting

--- src/main/java/com/example/blog_api/config/jwt/TokenProvider.java
+++ src/main/java/com/example/blog_api/config/jwt/TokenProvider.java
@@ -1,9 +1,39 @@
 package com.example.blog_api.config.jwt;
 
+import com.example.blog_api.domain.User;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
+
+
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Date;
 
 @RequiredArgsConstructor
 @Service
 public class TokenProvider {
+    private final JwtProperties jwtProperties;
+
+    public String generateToken(User user, Duration expiredAt){
+        Date now = new Date();
+        return makeToken(new Date(now.getTime() + expiredAt.toMillis()), user);
+    }
+
+    // JWT 토큰 생성 메서드
+    private String makeToken(Date expiry, User user) {
+        Date now = new Date();
+        return Jwts.builder()
+                .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+                .setIssuer(jwtProperties.getIssuer())
+                .setIssuedAt(now)
+                .setExpiration(expiry)
+                .setSubject(user.getEmail())
+                .claim("id", user.getId())
+                // 서명 : 비밀값과 해시값을 HS256 방식으로 암호화
+                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
+                .compact();
+    }
 }
